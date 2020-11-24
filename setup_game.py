5d25ea1d3c7eae628c53a5925c6f693024631c19
tesims62452 <tesims62452@outlook.com>
2020-11-24 09:40:00 -0500

    ?

--- setup_game.py
+++ setup_game.py
@@ -89,7 +89,7 @@ class MainMenu(input_handlers.BaseEventHandler):
         console.print(
             console.width // 2,
             console.height - 2,
-            "By me",
+            "By (Your name here)",
             fg=color.menu_title,
             alignment=tcod.CENTER,
         )
